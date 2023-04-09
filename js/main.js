3dc5d05eed8b3807eb12f4a1b110c199da2e1451
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-04-10 06:30:16 +0900

    deploy accordion js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,22 @@
 
 
 const elsTabsItem = document.querySelectorAll('.tabs__item'),
-elsTabsPanel = document.querySelectorAll('.tabs__panel'),
-elsTabLink = document.querySelectorAll('.js-tab-link');
+     elsTabsPanel = document.querySelectorAll('.tabs__panel'),
+     elsTabLink = document.querySelectorAll('.js-tab-link'),
+     elsAccordionItem = document.querySelectorAll('.accordion__item'),
+     elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+
 
 function deactivateTabsItem() {
      elsTabsItem.forEach(function (elTabsItem) {
           elTabsItem.classList.remove('tabs__item--active');
      });
 }
+function closeAccordionItems () {
+     elsAccordionItem.forEach(function (elsAccordionItem) {
+          elsAccordionItem.classList.remove('accordion__item--open');
+      });
+ }
 function deactivateTabPanels() {
      elsTabsPanel.forEach(function (elTabsPanel) {
           elTabsPanel.classList.remove('tabs__panel--active');
@@ -28,5 +36,14 @@ elsTabLink.forEach(function (elTabLink) {
           // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
           const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
           elTargetPanel.classList.add('tabs__panel--active');
+     });
+});
+
+
+elsAccordionItemToggler.forEach(function (elsAccordionItemToggler) {
+     elsAccordionItemToggler.addEventListener('click', function () {
+          closeAccordionItems();
+
+          elsAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
      });
 });
